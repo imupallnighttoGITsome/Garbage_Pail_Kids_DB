389847df1659a5c6dd3164c85d7d3148fe3d8d1a
imupallnighttogitsome <artishard.palmer@gmail.com>
2021-06-26 23:15:39 -0500

    working on connecting my css stylesheet

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
+
 
 const cardController = require('./controllers/cardcontroller')
 const ejsLayouts = require('express-ejs-layouts')
-require('dotenv').config()
+//require('dotenv').config()
 app.set('view engine', 'ejs')
 app.use(ejsLayouts)
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'));
 app.use('/gpk', cardController)
 
 
