5e68b1c6edb783bf228a456788c2605263b9afd6
imupallnighttogitsome <artishard.palmer@gmail.com>
2021-06-28 11:58:59 -0500

    working on card flip and layout

--- db/cardseeds.js
+++ db/cardseeds.js
@@ -1,14 +1,14 @@
 const Card = require('../models/cardmodel')
 const seedData = require('./cardseeds.json')
 
-// Card.deleteMany()
-//     .then(() => {
-//         return Card.insertMany(seedData)
-//     })
-//     .then(console.log)
-//     .catch(console.error)
-//     .finally(() => {
-//         process.exit()
-//     })
+Card.deleteMany()
+    .then(() => {
+        return Card.insertMany(seedData)
+    })
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => {
+        process.exit()
+    })
 
 module.exports = Card
