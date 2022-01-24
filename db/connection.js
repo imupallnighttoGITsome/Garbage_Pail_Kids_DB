72972abc9524498d9ee45c1312dd7f6eb7ef2d5b
imupallnighttogitsome <artishard.palmer@gmail.com>
2022-01-24 15:51:25 -0600

    .

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 const mongoURI = process.env.NODE_ENV === "production"
     ? process.env.DB_URL
-    : 'mongodb+srv://cpalmer84:jalapenos84@GPKCARDS.4g5kj.mongodb.net/gpk?retryWrites=true&w=majority'
+    : 'mongodb://localhost/gpkcardsdb';
     mongoose.connect((mongoURI), {
        useNewUrlParser: true,
        useCreateIndex: true,
