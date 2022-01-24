64758350a19b9aa69467c1fd1f8fc0ee29c49e30
imupallnighttogitsome <artishard.palmer@gmail.com>
2022-01-24 17:02:04 -0600

    .

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
-
-
-
-const cardController = require('./controllers/cardcontroller')
 const ejsLayouts = require('express-ejs-layouts')
-app.use(methodOverride('_method'))
+
+
+
 app.set('view engine', 'ejs')
 app.use(ejsLayouts)
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(__dirname + '/public'))
+app.use(methodOverride('_method'))
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+
+const cardController = require('./controllers/cardcontroller')
 app.use('/gpk', cardController)
-
-
 
 app.set('port', process.env.PORT || 4000)
 app.listen(app.get('port'), () => {
