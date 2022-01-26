d1455f6c1b8f0a749ad04d355fba1c17b272a991
imupallnighttogitsome <artishard.palmer@gmail.com>
2022-01-26 16:45:55 -0600

    reformatted index

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const methodOverride = require('method-override')
 const ejsLayouts = require('express-ejs-layouts')
 const cardController = require('./controllers/cardcontroller')
 
-
-
+//configs
+app.use(cors())
 app.set('view engine', 'ejs')
 app.use(ejsLayouts)
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
+
+//set up static files
 app.use(express.static(__dirname + '/public'))
 app.get('/', (req, res) => {
-    res.send('hello')
+    res.redirect('/gpkcards')
 })
 
-
+//controllers
 app.use('/gpkcards', cardController)
 
-app.set('port', process.env.PORT || 4000)
+//port
+app.set('port', process.env.PORT || 3000)
 app.listen(app.get('port'), () => {
     console.log(`in good shape on PORT: ${app.get('port')}`)
 
