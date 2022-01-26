216b71070a080a8c8c07e1201084842b57053d7a
imupallnighttogitsome <artishard.palmer@gmail.com>
2022-01-26 16:34:13 -0600

    i

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 app.use(express.static(__dirname + '/public'))
 app.get('/', (req, res) => {
-    res.redirect('/gpkcards')
+    res.render('index')
 })
 
 
