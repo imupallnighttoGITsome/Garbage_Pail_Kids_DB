46f8f1ec580d0199633952862074e1826599db39
imupallnighttogitsome <artishard.palmer@gmail.com>
2022-01-26 21:30:39 -0600

    setting up env

--- db/connection.js
+++ db/connection.js
@@ -10,8 +10,6 @@ const mongoURI = process.env.NODE_ENV === "production"
        useCreateIndex: true,
        useUnifiedTopology: true,
        useFindAndModify: false,
-      //  bufferMaxEntries: 0,
-      //  bufferCommands: false
     })
     .then(instance => {
        console.log(`Connected to db: ${instance.connections[0].name}`) 
